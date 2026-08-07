e13aa38df9a07569ca7c40b5d57a9c0c11cba302
Ashish Sharma Param Sharma <sharma@mac.lan>
2026-08-06 20:03:43 -0400

    Updates

---
+++ server/__vite_rsc_assets_manifest.js
@@ -0,0 +1,48 @@
+export default {
+  "bootstrapScriptContent": "import(\"/assets/index-Cw2h8-g5.js\")",
+  "clientReferenceDeps": {
+    "593f344dc510": {
+      "js": [
+        "/assets/index-Cw2h8-g5.js",
+        "/assets/rolldown-runtime-S-ySWqyJ.js",
+        "/assets/framework-XU8gKkFC.js"
+      ],
+      "css": []
+    },
+    "15c18cfaeeff": {
+      "js": [
+        "/assets/layout-segment-context-8Ok_l4CT.js",
+        "/assets/rolldown-runtime-S-ySWqyJ.js",
+        "/assets/index-Cw2h8-g5.js",
+        "/assets/framework-XU8gKkFC.js"
+      ],
+      "css": []
+    },
+    "c2747888630f": {
+      "js": [
+        "/assets/link-CyMQsVDE.js",
+        "/assets/rolldown-runtime-S-ySWqyJ.js",
+        "/assets/index-Cw2h8-g5.js",
+        "/assets/framework-XU8gKkFC.js",
+        "/assets/query-BbOc3VB2.js"
+      ],
+      "css": []
+    },
+    "8c0f216c4604": {
+      "js": [
+        "/assets/index-Cw2h8-g5.js",
+        "/assets/rolldown-runtime-S-ySWqyJ.js",
+        "/assets/framework-XU8gKkFC.js"
+      ],
+      "css": []
+    }
+  },
+  "serverResources": {
+    "app/layout.tsx": {
+      "js": [],
+      "css": [
+        "/assets/index-BeaY90By.css"
+      ]
+    }
+  }
+}
