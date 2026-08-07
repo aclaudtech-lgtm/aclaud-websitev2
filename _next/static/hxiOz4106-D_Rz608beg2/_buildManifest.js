08ae0b10e6f31d6d467b8a91fa82d21464ed2c69
Ashish Sharma Param Sharma <sharma@mac.lan>
2026-08-06 21:21:25 -0400

    Updates

--- _next/static/hxiOz4106-D_Rz608beg2/_buildManifest.js
+++ _next/static/hxiOz4106-D_Rz608beg2/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/aclaud-websitev2//_next/:path+",
-        "destination": "/aclaud-websitev2/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
